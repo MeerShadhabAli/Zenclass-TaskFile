1afd733fafcdb9fc44ac11c8c81442b800de83e8
Meershadhap04 <shathapmeer04@email.com>
2024-03-20 18:25:52 +0530

    Task Completed

---
+++ Javascript Day6 Task/uber.js
@@ -0,0 +1,11 @@
+class Uber {
+    constructor(carModel, cartype, seatCapacity, kms) {
+        this.carModel=carModel;
+        this.cartype=cartype;
+        this.seatCapacity=seatCapacity;
+        this.kms=kms;
+    }
+}
+var ride1=new Uber("Innova", "MUV", "7-Seater", 30);
+var ride2=new Uber("Etios", "Sedan", "4-Seater", 200);
+console.log(ride1);
